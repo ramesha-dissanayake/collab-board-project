09036e2df3f88f2ce81f6cf41d74aa75177f3ea7
Tharun Sasanka <tharunsasanka0@gmail.com>
2026-08-07 10:15:01 +0530

    Create dynamic Column component and implement in Board

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <header className="app-header">
+        <h1>CollabBoard Workspace</h1>
+        <p>Project Management Dashboard</p>
+      </header>
+      
+      <main className="board-container">
+        {/* The Board component built by Squad 2 will be inserted right here later */}
+        <p>The Kanban Board will render here...</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
