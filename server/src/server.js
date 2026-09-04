38f83d3bd4bb214d63701dc5ccf005cb922f0f5a
ramesha-dissanayake <isurangi66@gmail.com>
2026-09-04 22:44:01 +0530

    feat: add MongoDB connection and health status

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,32 @@
+import process from "node:process";
+
 import app from "./app.js";
 import { config } from "./config.js";
+import { connectDb } from "./db/connect.js";
 
-app.listen(config.port, () => {
-  console.log(`CollabBoard API running at http://localhost:${config.port}`);
-});
+async function startServer() {
+  try {
+    await connectDb();
+
+    app.listen(
+      config.port,
+      () => {
+        console.log(
+          `CollabBoard API running at http://localhost:${config.port}`
+        );
+      }
+    );
+  } catch (error) {
+    console.error(
+      "Failed to start CollabBoard API:"
+    );
+
+    console.error(
+      error.message
+    );
+
+    process.exit(1);
+  }
+}
+
+startServer();
